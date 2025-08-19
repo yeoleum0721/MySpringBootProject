e9ca43fce94fa38d7ed14b2585b246adeee7e391
yeoleum0721 <jooyun0229@gmail.com>
2025-08-19 14:10:53 +0900

    security config

--- src/main/java/com/rookies4/myspringboot/security/config/SecurityConfig.java
+++ src/main/java/com/rookies4/myspringboot/security/config/SecurityConfig.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -14,4 +18,18 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    @Bean
+//authentication(인증)을 위한 User 생성 (관리자, 일반사용자)
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        UserDetails admin = User.withUsername("adminboot")
+                .password(encoder.encode("pwd1"))
+                .roles("ADMIN")
+                .build();
+        UserDetails user = User.withUsername("userboot")
+                .password(encoder.encode("pwd2"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
 }
