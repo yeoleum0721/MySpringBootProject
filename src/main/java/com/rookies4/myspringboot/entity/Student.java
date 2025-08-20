c296cebbf76971fd82d804394efb758be4f8d0fe
yeoleum0721 <jooyun0229@gmail.com>
2025-08-20 16:10:37 +0900

    student entity joincolumn

--- src/main/java/com/rookies4/myspringboot/entity/Student.java
+++ src/main/java/com/rookies4/myspringboot/entity/Student.java
@@ -14,9 +14,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class Student {
-    
+    //PK(Sequential Value)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
     private Long id;
     
     @Column(nullable = false)
--- src/main/java/com/rookies4/myspringboot/entity/StudentDetail.java
+++ src/main/java/com/rookies4/myspringboot/entity/StudentDetail.java
@@ -16,22 +16,31 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Builder
 public class StudentDetail {
-    
+
+    //PK(Sequential value)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="student_detail_id")
     private Long id;
-    
+
+    //주소
     @Column(nullable = false)
     private String address;
-    
+
+    //전화번호
     @Column(nullable = false)
     private String phoneNumber;
-    
-    @Column
+
+    //이메일
+    @Column(unique = true,nullable = false)
     private String email;
-    
+
+    //생년월일
     @Column
     private LocalDate dateOfBirth;
-    
+
+    //student의 PK와 연결
+    //JoinColumn은 FK에 해당하는 어노테이션
+    @JoinColumn(name = "student_id",unique = true)
     private Student student;
 }
